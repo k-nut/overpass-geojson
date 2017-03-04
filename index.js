b9674da6c64621e95d1424f56df7dfbd9a62d621
Knut Hühne <knut@k-nut.eu>
2017-03-04 10:46:05 +0100

    do not crash so easily

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 let http = require('http');
-// let osmtogeojson = require('osmtogeojson');
+let osmtogeojson = require('osmtogeojson');
+const _ = require('lodash');
 
 
 console.log('Loading function');
@@ -21,36 +22,41 @@ exports.handler = (event, context, callback) => {
           path: '/cgi/interpreter?data=' + encodeURIComponent(queryString),
         };
 
-        //done(null, options);
-        // return;
-
         let callback = function(response) {
-          var str = '';
+          let str = '';
 
           response.on('data', function (chunk) {
             str += chunk;
           });
 
           response.on('end', function () {
-              console.log(str);
-              var data = "";
+              let data = "";
               try{
                   data = JSON.parse(str);
+                  done(null, osmtogeojson(data));
               }
               catch(err){
-                 data = str;
+                 console.log("ERROR OCCURED");
+                 done(new Error(str));
               }
-            //console.log(osmtogeojson(data));;
-            done(null, data);
           });
         };
 
         http.request(options, callback).end();
     };
 
+    const handleGet = function(event){
+      const data = _.get(event, "queryStringParameters.data");
+      if (data){
+          callOverpass(data);
+      } else {
+        done(new Error("You need to supply a `data` parameter"));
+      }
+    };
+
     switch (event.httpMethod) {
         case 'GET':
-            callOverpass(event.queryStringParameters.data);
+            handleGet(event);
             break;
         default:
             done(new Error(`Unsupported method "${event.httpMethod}"`));
